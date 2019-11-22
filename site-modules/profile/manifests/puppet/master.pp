2647ad6c2f81604e14121e2ab8070c5a579494d7
Gabriel M Schuyler <gabe@puppetlabs.com>
2019-11-22 09:44:17 -0700

    Add firewall rule for port 8140 to profile::puppet::master

--- site-modules/profile/manifests/puppet/master.pp
+++ site-modules/profile/manifests/puppet/master.pp
@@ -5,4 +5,23 @@ class profile::puppet::master {
     proto => 'tcp',
     addr  => '8170',
   }
+
+  # Add a firewall rule so folks can reach me on port 8140.
+  file { '/etc/firewalld/services/puppetmaster.xml':
+    ensure => file,
+    source => 'puppet:///modules/profile/firewalld/puppetmaster.xml',
+    notify => Exec['add_firewalld_rule'],
+  }
+  exec { 'add_firewalld_rule':
+    path        => $facts['path'],
+    command     => 'firewall-cmd --permanent --add-service=puppetmaster',
+    refreshonly => true,
+    notify      => Exec['firewalld_reload'],
+  }
+  exec { 'firewalld_reload':
+    path        => $facts['path'],
+    command     => 'firewall-cmd --reload',
+    refreshonly => true,
+  }
+
 }
